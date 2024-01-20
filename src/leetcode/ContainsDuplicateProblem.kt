8891267a5a2e001e5ef19d3ec70860da9a815898
abhishektiwarijr <abhishektiwarigkp@gmail.com>
2024-01-20 23:47:54 +0530

    Leetcode contains duplicate - II

--- src/leetcode/ContainsDuplicateProblem.kt
+++ src/leetcode/ContainsDuplicateProblem.kt
@@ -6,6 +6,28 @@ fun main() {
     println(containsDuplicate(intArrayOf(1, 2, 3, 1)))
     println(containsDuplicate(intArrayOf(1, 2, 3, 4)))
     println(containsDuplicate(intArrayOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2)))
+
+    println(containsDuplicateII(intArrayOf(1, 2, 3, 1), k = 3)) //true
+    println(containsDuplicateII(intArrayOf(1, 0, 1, 1), k = 1)) //true
+    println(containsDuplicateII(intArrayOf(1, 2, 3, 1, 2, 3), k = 2)) //false
+}
+
+private fun containsDuplicateII(nums : IntArray, k : Int) : Boolean {
+    val table = hashMapOf<Int, Int>()
+
+    //if current number is in table then check for distance
+    //else keep putting number in the table
+    for (i in 0..nums.lastIndex) {
+        if(table.contains(nums[i])) {
+            val distance = (i - table[nums[i]]!!)
+            if(distance <= k) {
+                return true
+            }
+        }
+        table[nums[i]] = i
+    }
+
+    return false
 }
 
 private fun containsDuplicate(nums: IntArray): Boolean {
